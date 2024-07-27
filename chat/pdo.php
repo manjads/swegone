64e892f72a409d3ba085ef35f29a8cae31666bc4
Manjads <kimanjad29@gmail.com>
2024-07-28 01:50:15 +0800

    Update pdo.php

--- chat/pdo.php
+++ chat/pdo.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=pak_dewa', 'admin', 'admin');
+$pdo = new PDO('mysql:host=b7p.h.filess.io;port=3307;dbname=chat_goneobject', 'chat_goneobject', '25df92213bdbf09c565a5e69844e61ac5463e9de');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
