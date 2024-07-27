afb0cb629dd235b95a54af017fd4e29c5ff69247
Manjads <kimanjad29@gmail.com>
2024-07-28 01:51:57 +0800

    Update konek.php

--- chat/konek.php
+++ chat/konek.php
@@ -1,8 +1,8 @@
 <?php
-$host='localhost';
-$username='root';
-$pass='';
-$db='pak_dewa';
+$host='b7p.h.filess.io';
+$username='chat_goneobject';
+$pass='25df92213bdbf09c565a5e69844e61ac5463e9de';
+$db='chat_goneobject';
 
 $kon = mysqli_connect ($host,$username,$pass,$db);
 ?>
